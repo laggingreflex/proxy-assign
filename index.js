aecf1f3a9be6cafec14deca5f2576c8eba818021
unknown <root>
2022-01-16 05:50:01 +0530

    fix: look up own properties, then all

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = (target, ...sources) => {
 
   const lookupArray = Array.from(sources).reverse().concat([target]);
-  const findSourceOf = key => lookupArray.find(_ => typeof _?.[key] !== typeof undefined);
+  const findSourceOf = key =>
+    lookupArray.find(_ => _?.hasOwnProperty?.(key) && _) ?? // First look in own properties
+    lookupArray.find(_ => typeof _?. [key] !== typeof undefined); // then in all properties
 
   function get(target, key) {
     const source = findSourceOf(key) || target;
